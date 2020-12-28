30e18b000cab9c687706674de5e0034312b1593f
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2020-12-29 01:46:32 +0300

    End App_2

--- app_2/lib/main.dart
+++ app_2/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
 
   final List<String> _measures=[
-    'measures',
+    'meters',
     'kilometers',
     'grams',
     'kilograms',
@@ -19,10 +19,50 @@ class MyAppState extends State<MyApp> {
     'ounces',
   ];
 
+  final Map<String, int> _measuresMap={
+    'meters':0,
+    'kilometers':1,
+    'grams':2,
+    'kilograms':3,
+    'feet':4,
+    'miles':5,
+    'pounds (lbs)':6,
+    'ounces':7,
+  };
+
+  final dynamic _formulas={
+    '0':[1,0.001,0,0,3.28084,0.000621371,0,0],
+    '1':[1000,1,0,0,3280.84,0.621371,0,0],
+    '2':[0,0,1,0.0001,0,0,0.00220462,0.035274],
+    '3':[0,0,1000,1,0,0,2.20462,35.274],
+    '4':[0.3048,0.0003048,0,0,1,0.000189394,0,0],
+    '5':[1609.34, 1.60934,0,0,5280,1,0,0],
+    '6':[0,0,453.592,0.453592,0,0,1,16],
+    '7':[0,0,28.3495,0.0283495,3.28084,0,0.0625, 1],
+  };
+
   String _startMeasure;
   String _convertedMeasure;
 
   double _numberFrom;
+
+  String _resultMessage;
+
+  void convert(double value, String from, String to){
+      int nFrom=_measuresMap[from];
+      int nTo=_measuresMap[to];
+      var multiplier= _formulas[nFrom.toString()][nTo];
+      var result=value*multiplier;
+
+      if(result ==0){
+        _resultMessage='This conversion cannot be performed';
+      }else{
+        _resultMessage="${_numberFrom.toString()} $_startMeasure are ${result.toString()} $_convertedMeasure";
+      }
+      setState((){
+        _resultMessage=_resultMessage;
+      });
+    }
 
   final TextStyle inputStyle=TextStyle(//Text style for TextFields, 
   //DropDownButtons and Button
@@ -46,6 +86,7 @@ class MyAppState extends State<MyApp> {
 
         body: Container(
           padding:EdgeInsets.symmetric(horizontal: 20),
+          
           child:Column(
             children:[
             Spacer(),
@@ -115,13 +156,19 @@ class MyAppState extends State<MyApp> {
 
           RaisedButton(
             child:Text('Convert', style:inputStyle),
-            onPressed: ()=>{print("trying to convert")},
+            onPressed: (){
+              if (_startMeasure.isEmpty || _convertedMeasure.isEmpty || _numberFrom==0){
+                return ;
+              }else{
+                convert(_numberFrom, _startMeasure, _convertedMeasure);
+              }
+            },
           ),
 
           Spacer(flex:2,),
 
           Text(
-            (_numberFrom==null) ? '' : _numberFrom.toString(),
+            (_resultMessage==null) ? '' : _resultMessage,
             style:labelStyle
           ), 
 
@@ -131,6 +178,9 @@ class MyAppState extends State<MyApp> {
         )),
       ),
     );
+
+    //Logic for conversion
+    
   }
 
   @override
