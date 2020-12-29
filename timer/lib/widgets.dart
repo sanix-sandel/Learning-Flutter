43774261320d15d36f0b8630f06f0a8ebc87bd31
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2020-12-30 02:48:30 +0300

    timer done

--- timer/lib/widgets.dart
+++ timer/lib/widgets.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
-
-class ProductivityButton extends StatelessWidget{
+typedef CallbackSetting = void Function(String, int);
+class ProductivityButton extends StatelessWidget {
   final Color color;
   final String text;
   final double size;
   final VoidCallback onPressed;
 
-  ProductivityButton({@required this.color, @required this.text, 
-  @required this.onPressed, @required this.size});
-
+  ProductivityButton(
+      {@required this.color,
+      @required this.text,
+      @required this.onPressed,
+      this.size});
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialButton(
-      child:Text(
-          this.text,
-          style:TextStyle(color:Colors.white)
-        ),
-        onPressed: this.onPressed,
-        color:this.color,
-        minWidth: this.size,
-      );
-    
+      child: Text(this.text, style: TextStyle(color: Colors.white)),
+      onPressed: this.onPressed,
+      color: this.color,
+      minWidth: (this.size != null) ? this.size : 0,
+    );
+  }
+}
+
+class SettingsButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final int value;
+  final String setting;
+  final CallbackSetting callback;
+  SettingsButton(this.color, this.text, this.value, this.setting, this.callback);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      child: Text(this.text, style: TextStyle(color: Colors.white)),
+      onPressed: () => this.callback(this.setting, this.value),
+      color: this.color,
+    );
   }
 }
