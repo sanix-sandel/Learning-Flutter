6392521e19d91ec3468d6c4bc50ae1edcb25d73b
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2020-12-29 03:28:50 +0300

    Timer app 2.0

---
+++ timer/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import './widgets.dart';
+
+import 'package:percent_indicator/percent_indicator.dart';
+
+
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+
+  
+  
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title:'My work Timer',
+      theme:ThemeData(primarySwatch: Colors.blueGrey,),
+      home:TimerHomePage(),
+
+      
+    );
+  }
+
+  
+}
+
+class TimerHomePage extends StatelessWidget{
+
+  final double defaultPadding=5.0;
+
+  void emptyMethod(){}
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(appBar: AppBar(title:Text('My work timer')), 
+    
+                    body: LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints)
+                      {
+                        final double availableWidth=constraints.maxWidth;
+                        return Column(children: [
+                      Row(
+                        children: [
+                          Padding(padding: EdgeInsets.all(defaultPadding),),
+                          Expanded(child: ProductivityButton(color: Color(0xff009688),
+                          text: "Work", onPressed: emptyMethod)),
+                          Padding(padding: EdgeInsets.all(defaultPadding),),
+                          Expanded(child: ProductivityButton(color: Color(0xff607D8B),
+                          text: "Short Break", onPressed: emptyMethod)),
+                          Padding(padding: EdgeInsets.all(defaultPadding),),
+                          Expanded(child: ProductivityButton(color: Color(0xff455A64),
+                          text: "Long Break", onPressed: emptyMethod)),
+                          Padding(padding: EdgeInsets.all(defaultPadding),),
+                          ],
+                        ),
+                      
+
+                        Expanded(
+                            child: CircularPercentIndicator(
+                            radius: availableWidth / 2,
+                            lineWidth: 10.0,
+                            percent: 1,
+                            center: Text("30:00",
+                            style: Theme.of(context).textTheme.display1),
+                            progressColor: Color(0xff009688),
+                          )),
+
+                        Row(children: [
+                            Padding(padding: EdgeInsets.all(defaultPadding),),
+                            Expanded(child: ProductivityButton(color: Color(0xff212121),
+                        text: 'Stop', onPressed: emptyMethod)),
+                            Padding(padding: EdgeInsets.all(defaultPadding),),
+                            Expanded(child: ProductivityButton(color: Color(0xff009688),
+                        text: 'Restart', onPressed: emptyMethod)),
+                          Padding(padding: EdgeInsets.all(defaultPadding),),
+                        ],)
+                    ]);}
+    
+    ));
+  }
+}
