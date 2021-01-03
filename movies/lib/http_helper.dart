e40b587747a3d1d7959dbc4859c9e2666cddfffc
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2021-01-03 14:38:30 +0300

    displaying movies with ListTile without image

--- movies/lib/http_helper.dart
+++ movies/lib/http_helper.dart
@@ -1,5 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'dart:io';
+import 'dart:convert';
+import 'movie.dart';
 
 class HttpHelper {
   final String urlKey = 'api_key=dc3fc3229cfc3359a79dab50d2b96a77';
@@ -7,12 +9,14 @@ class HttpHelper {
   final String urlUpcoming = '/upcoming?';
   final String urlLanguage = '&language=en-US';
 
-  Future<String> getUpcoming() async {
+  Future<List> getUpcoming() async {
     final String upcoming = urlBase + urlUpcoming + urlKey + urlLanguage;
     http.Response result = await http.get(upcoming);
     if (result.statusCode == HttpStatus.ok) {
-      String responseBody = result.body;
-      return responseBody;
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
     } else {
       return null;
     }
--- movies/lib/movie_list.dart
+++ movies/lib/movie_list.dart
@@ -10,6 +10,18 @@ class _MovieListState extends State<MovieList> {
   String result;
   HttpHelper helper;
 
+  int moviesCount;
+  List movies;
+
+  Future initialize() async {
+    movies = List();
+    movies = await helper.getUpcoming();
+    setState(() {
+      moviesCount = movies.length;
+      movies = movies;
+    });
+  }
+
   @override
   void initState() {
     helper = HttpHelper();
@@ -18,16 +30,23 @@ class _MovieListState extends State<MovieList> {
 
   @override
   Widget build(BuildContext context) {
-    helper.getUpcoming().then((value) {
-      setState(() {
-        result = value;
-      });
-    });
-
     return Scaffold(
         appBar: AppBar(
           title: Text('Movies'),
         ),
-        body: Container(child: Text(result)));
+        body: ListView.builder(
+          itemCount: (this.moviesCount == null) ? 0 : this.moviesCount,
+          itemBuilder: (BuildContext context, int position) {
+            return Card(
+                color: Colors.white,
+                elevation: 2.0,
+                child: ListTile(
+                    title: Text(movies[position].title),
+                    subtitle: Text('Released: ' +
+                        movies[position].releaseDate +
+                        ' -Vote: ' +
+                        movies[position].voteAverage.toString())));
+          },
+        ));
   }
 }
